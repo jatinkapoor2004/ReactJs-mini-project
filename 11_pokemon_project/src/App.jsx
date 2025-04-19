39ed7a156b428eafe4374c1f46365315afb1bace
Jatin Kapoor <Jatinkapoor3066@gmail.com>
2025-04-19 13:25:45 +0530

    Pokemon Game

--- 11_pokemon_project/src/App.jsx
+++ 11_pokemon_project/src/App.jsx
@@ -2,29 +2,73 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import getAllPokemonData from './hooks/usePokemonApi'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const alldata = getAllPokemonData()
+  // console.log(alldata);
 
+  const allPokemon = getAllPokemonData();
+  const [randomPoke, setRandomPoke] = useState(null);
+  const [pokeImage, setPokeImage] = useState("");
+
+
+  // function getpoke() 
+  // {
+  //    console.log( typeof alldata + " from app file  ");
+  // }
+  // getpoke();
+
+
+
+  function getPokemonName() {
+    if (allPokemon.length == 0) return;
+
+    const pokeNumber = Math.floor(Math.random() * 1025);
+    console.log(pokeNumber);
+
+    let name = allPokemon[pokeNumber];
+    console.log(name);
+
+    if (!name) return;
+    name = name.name;
+    setRandomPoke(name);
+    setPokeImage(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNumber+1}.png`)
+    console.log(name);
+    return name;
+  }
+
+  // function getPokemonPhoto() {
+  //   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNumber}.png`;
+  //   fetch(url)
+  //     .then((res) => { return res });
+  // }
   return (
     <>
       <div className='flex justify-center items-center h-screen bg-zinc-600'>
-          <div className='border border-black rounded-2xl bg-gray-400 w-2/3 h-3/5'>
-              <div className='flex justify-center mt-4'>
-                  <button 
-                    type="submit"
-                    className='border border-none px-4 py-2 rounded-md bg-blue-600 text-white'
-                  >Fet Random Pokemon </button>
-              </div> 
-
-              <div className='flex justify-center mt-3  p-1 text-xl text-gray-950 '>
-                  Name will be shown here 
-              </div>
-              <div className='flex justify-center items-center mt-16'>
-                Image will be shown here  
-              </div>
-
+        <div className='border border-black rounded-2xl bg-gray-400 w-2/3 h-3/5'>
+          <div className='flex justify-center mt-4'>
+            <button
+              type="submit"
+              className='border border-none px-6 py-4 rounded-md bg-blue-600 text-white text-3xl'
+              onClick={getPokemonName}
+            >Fet Random Pokemon </button>
           </div>
+
+          <div className='flex justify-center mt-6  p-1 text-3xl text-gray-950 '>
+              {randomPoke && randomPoke.toUpperCase()}
+          </div>
+
+          <div className='flex justify-center items-center mt-16'>
+            {/* <img src={pokeImage} alt={"Error in Image"} /> */}
+            {pokeImage && <img 
+            src={pokeImage} 
+            alt={randomPoke}
+            className="w-48 h-48 scale-150"
+            />}
+          </div>
+
+        </div>
       </div>
     </>
   )
--- 11_pokemon_project/src/hooks/usePokemonApi.js
+++ 11_pokemon_project/src/hooks/usePokemonApi.js
@@ -1,11 +1,39 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 let url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
 
-const[allData,setAllData] = useState({});
+function getAllPokemonData() {
+    const [allData, setAllData] = useState([]);
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            // .then((res) => setAllData(res.results))
+            .then((data) => {
+                setAllData(data.results); // ✅ get the array from 'results' key
+            })
+            .catch((err) => console.error("Failed to fetch pokemon data from main API",err));
+            
+        //console.log(allData);
+    },[])
+    return allData;
+}
+// function getPokemonName() {
+//     let pokeNumber1 = Math.floor(Math.random() * 1301);
+//     setPokeNumber(pokeNumber1);
 
-useEffect(() =>{
-    fetch(url)
-    .then((res) => res.json())
-    .then((res) => setAllData(res[results]))
-})
+//     const name = allData[pokeNumber]["name"];
+//     console.log(pokeNumber);
+
+//     console.log(name);
+//     return name;
+// }
+// function getPokemonPhoto() {
+//     const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNumber}.png`;
+//     fetch(url)
+//         .then((res) => { return res });
+// }
+
+export default getAllPokemonData;
+
+
+
